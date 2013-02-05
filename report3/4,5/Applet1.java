c45125d070b0e76c633496b35472418dd1621d73
innparusu <e125716@gmail.com>
2013-02-05 23:40:34 +0900

    Applet1  add

--- report3/4,5/Applet1.java
+++ report3/4,5/Applet1.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class Applet1 extends Applet {
   public void paint(Graphics g){
-    int[] x = {100,50,150};
-    int[] y = {50,100,100};
-    g.drawPolygon(x,y,3);
+    int[] x = {100,50,150}; //描画する図形のx座標を指定
+    int[] y = {50,100,100}; //描画する図形のy座標を指定
+    g.drawPolygon(x,y,3); //配列x,yと3を引数とし,三角形を描画
   }
 }
