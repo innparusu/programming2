b12e944865769fa0079253c723868b88d699b16b
innparusu <e125716@gmail.com>
2013-02-09 11:51:13 +0900

    Applet3.java add

---
+++ report3/4,5/Applet3.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+
+public class Applet3 extends Applet {
+  public void init(){
+    Button a = new Button("ボタンa"); //ボタンのインスタンスを作成する。インスタンス名をaとする。
+    add(a); //ボタンをアプレットに追加する。
+  }
+}
