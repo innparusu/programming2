5e59bd24269470c4035f98056ce7523cadb7e573
innparusu <e125716@gmail.com>
2013-02-09 11:52:48 +0900

    Applet3.java add

--- report3/4,5/Applet3.java
+++ report3/4,5/Applet3.java
@@ -2,7 +2,7 @@ import java.applet.*;
 import java.awt.*;
 
 public class Applet3 extends Applet {
-  public void init(){
+  public void init(){ //paint()メソッドではなくinit()メソッドに記述
     Button a = new Button("ボタンa"); //ボタンのインスタンスを作成する。インスタンス名をaとする。
     add(a); //ボタンをアプレットに追加する。
   }
